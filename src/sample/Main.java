8ced8ddb7416e2f37984c7ca6d8493e17bee99ca
Bernbark <korystennett@yahoo.com>
2022-03-13 01:00:41 -0800

    Making Hand cards reversable, cleaning up code to condense in a for loop.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,40 +16,55 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.util.Random;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        final Card card = new Card(1);
+        int max = 10;
+        int min = 1;
 
+        final Card cards[] = new Card[8];
+        Button reverseSignButtons[] = new Button[8];
+        StackPane cardSlots[] = new StackPane[8];
+        GridPane cardOrganizerPanes[] = new GridPane[8];
 
-        Button button = new Button("Reverse Sign");
-        button.setOnAction(new EventHandler() {
-            @Override
-            public void handle(Event event) {
-                ReverseSign(card);
-            }
+        for(int i = 0; i < cards.length; i++){
+            cardSlots[i] = new StackPane();
+            cardOrganizerPanes[i] = new GridPane();
+            cardOrganizerPanes[i].setPadding(new Insets(10,10,10,10));
+            Random rand = new Random();
+            int randomNum = rand.nextInt((max - min) + 1) + min;
+            cards[i] = new Card(randomNum);
+            cards[i].SetText(MakeText(cards[i]));
+            cards[i].SetRectangle(MakeRectangle());
+            final int index = i;
+            reverseSignButtons[i] = new Button("Reverse Sign");
+            reverseSignButtons[i].setOnAction(new EventHandler() {
+                @Override
+                public void handle(Event event) {
+                    ReverseSign(cards[index]);
+                }
+            });
+            cardOrganizerPanes[i].add(cards[i].text, 0,0,1,1);
+            cardOrganizerPanes[i].add(reverseSignButtons[i], 0,2,1,1);
+            cardSlots[i].getChildren().addAll(cards[i].rectangle, cardOrganizerPanes[i]);
+        }
 
+        //StackPane cardSlotOne = new StackPane();
+        //cardSlotOne.getChildren().addAll(card.rectangle,card.text, button);
 
-        });
+        //StackPane cardSlotTwo = new StackPane();
+        //Rectangle rectangle1 = MakeRectangle();
 
-        card.SetText(MakeText(card));
-        card.SetRectangle(MakeRectangle());
-
-        StackPane cardSlotOne = new StackPane();
-        cardSlotOne.getChildren().addAll(card.rectangle,card.text, button);
-
-        StackPane cardSlotTwo = new StackPane();
-        Rectangle rectangle1 = MakeRectangle();
-
-        Card card1 = new Card(2);
-        Text text1 = MakeText(card1);
-        text1.setText(card1.GetValueAsString());
-        card1.SetText(text1);
-        card1.SetRectangle(rectangle1);
-        cardSlotTwo.getChildren().addAll(rectangle1,text1,button);
+        //Card card1 = new Card(2);
+        //Text text1 = MakeText(card1);
+        //text1.setText(card1.GetValueAsString());
+        //card1.SetText(text1);
+        //card1.SetRectangle(rectangle1);
+        //cardSlotTwo.getChildren().addAll(rectangle1,text1,button);
 
         /**
          * Construction of Player 1's Hand UI
@@ -58,19 +73,23 @@ public class Main extends Application {
         playerOneHand.setVgap(10);
         playerOneHand.setHgap(10);
         playerOneHand.setPadding(new Insets(10,10,10,10));
-        playerOneHand.add(cardSlotOne, 0, 0,1, 1);
-        playerOneHand.add(cardSlotTwo,1,0,1,1);
+        playerOneHand.add(cardSlots[0], 0, 0,1, 1);
+        playerOneHand.add(cardSlots[1],1,0,1,1);
+        playerOneHand.add(cardSlots[2],2,0,1,1);
+        playerOneHand.add(cardSlots[3],3,0,1,1);
 
         GridPane playerTwoHand = new GridPane();
         playerTwoHand.setVgap(10);
         playerTwoHand.setHgap(10);
         playerTwoHand.setPadding(new Insets(10,10,10,10));
-        playerTwoHand.add(cardSlotOne, 0, 0,1, 1);
-        playerTwoHand.add(cardSlotTwo,1,0,1,1);
+        playerTwoHand.add(cardSlots[4], 0, 0,1, 1);
+        playerTwoHand.add(cardSlots[5],1,0,1,1);
+        playerTwoHand.add(cardSlots[6],2,0,1,1);
+        playerTwoHand.add(cardSlots[7],3,0,1,1);
 
         GridPane playingField = new GridPane();
-        playingField.add(playerOneHand,0,0,2,1);
-        playingField.add(playerTwoHand,2,0, 2,1);
+        playingField.add(playerOneHand,0,0,4,1);
+        playingField.add(playerTwoHand,4,0, 4,1);
 
         Scene scene = new Scene(playingField, 1024, 800, true);
 
